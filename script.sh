82eae718556f42359a9dcd28cab3c2420c2d2615
TEchRExBOis <56177091+TEchRExBOis@users.noreply.github.com>
2023-06-19 22:19:20 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-my_array=($(az storage blob list --account-name `$1` --container-name `$2` --prefix "" --only-show-errors | jq -r '.[].name'))
+my_array=($(az storage blob list --account-name $1 --container-name $2 --prefix "" --only-show-errors | jq -r '.[].name'))
 # Read the input string
 read -p "Enter the filter string: " filter_string
 
